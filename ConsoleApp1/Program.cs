5e35afe834870943df952e341b2a315721210e5c
jitendra gupta <jitendra.gupta41@gmail.com>
2020-12-07 15:32:19 +0530

    push and sync

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("heyy Hello");
+            Console.WriteLine("heyy Hello1");
         }
     }
 }
